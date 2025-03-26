0c75d94c8fbae5a0ed29e1c1350f2c8a037028b4
Mayuri-Varshney <155051840+Mayuri-Varshney@users.noreply.github.com>
2025-03-26 22:51:58 +0530

    Delete src directory

--- src/main/java/com/theblogdemo/blog/rest/api/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.theblogdemo.blog.rest.api.repository;
-import com.theblogdemo.blog.rest.api.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByPostId(Long postId);
-}
--- src/main/java/com/theblogdemo/blog/rest/api/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.theblogdemo.blog.rest.api.repository;
-
-import com.theblogdemo.blog.rest.api.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
-
